ef622bde787d63bf9df9ea0ba23240ad1b6b673d
Glitch (cerlancism-greatleapdemodev) <none>
2019-04-08 05:22:47 +0000

    🚅😬 Checkpoint

--- src/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const server = require('./server')
-const bot = require('./discordBot')
-
-server.withBot(bot)
-bot.withServer(server)
-
-console.log("Discord Bot Token", process.env.TOKEN)
